10aace004c816a3c19b97981afd18c99ce031da9
Nikolas Santis <nikosantis@gmail.com>
2020-01-05 11:34:00 -0300

    fetch try catch async await

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,10 +2,14 @@ import axios from 'axios'
 import { GET_ALL } from '../types/usersTypes'
 
 export const getAll = () => async dispatch => {
-  const fetchData = await axios.get('https://jsonplaceholder.typicode.com/users')
+  try {
+    const fetchData = await axios.get('https://jsonplaceholder.typicode.com/users')
+    dispatch({
+      type: GET_ALL,
+      payload: fetchData.data
+    })
+  } catch (error) {
+    console.log('Error:', error.message)
+  }
 
-  dispatch({
-    type: GET_ALL,
-    payload: fetchData.data
-  })
 }
