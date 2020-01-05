6ee5927f0c74143ecb253b6dd06e1bb797dba0af
Nikolas Santis <nikosantis@gmail.com>
2020-01-05 20:19:54 -0300

    Mapping objects

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Loading } from '../General/Loading'
+import { Fatal } from '../General/Fatal'
+
 import * as tasksActions from '../../actions/tasksActions'
 
 class Tasks extends Component {
@@ -8,10 +11,47 @@ class Tasks extends Component {
     this.props.getAll()
   }
 
+  showContent = () => {
+    const { tasks, loading, error } = this.props
+
+    if (loading) {
+      return <Loading />
+    }
+
+    if (error) {
+      return <Fatal message={error} />
+    }
+
+    return Object.keys(tasks).map(user_id => (
+      <div key={user_id}>
+        <h2>Usuario {user_id}</h2>
+        <div className='tasks_container'>
+          { this.putTasks(user_id) }
+        </div>
+      </div>
+    ))
+  }
+
+  putTasks = user_id => {
+    const { tasks } = this.props
+    const by_user = {
+      ...tasks[user_id]
+    }
+
+    return Object.keys(by_user).map(tsk_id => (
+      <div key={tsk_id}>
+        <input type='checkbox' defaultChecked={by_user[tsk_id].completed} />
+        {
+          by_user[tsk_id].title
+        }
+      </div>
+    ))
+  }
+
   render() {
     console.log(this.props)
     return (
-      <div>Tareas saludar</div>
+      <div>{ this.showContent() }</div>
     )
   }
 }
