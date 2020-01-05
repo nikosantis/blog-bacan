dfa18603eebbec3d2a208a6e8e0f377b41c13a0f
Nikolas Santis <nikosantis@gmail.com>
2020-01-05 14:52:20 -0300

    usersActions in posts component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as usersActions from '../../actions/usersActions'
 
 class Posts extends Component {
+  componentDidMount() {
+    if (!this.props.users.length) {
+      this.props.getAll()
+    }
+  }
+
+
   render() {
+    console.log(this.props)
     return (
       <div>
+        <h1>Publicaciones de</h1>
         { this.props.match.params.key }
       </div>
     )
   }
 }
 
-export default Posts
+const mapStateToProps = reducers => {
+  return reducers.usersReducer
+}
+
+export default connect(mapStateToProps, usersActions)(Posts)
